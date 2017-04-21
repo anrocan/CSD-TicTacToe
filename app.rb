ad94b0d7497bfadfaf84dc1120afa2b4840f981e
anrocan <vagrant@kleer-csd-box>
2017-04-21 16:14:21 -0300

    Final1

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'sinatra'
+require './lib/Grid'
 
 @@version=0.1
 @@buttonPressed=nil
+@@currentPlayer=nil
 
 get '/' do
   @@version=0.1
+  @@currentPlayer
   erb :index
 end
 
 post '/:boton' do |boton|
   @@version=0.1
   @@buttonPressed = boton
-  erb :index
+
+    erb :index
 end
+
+
